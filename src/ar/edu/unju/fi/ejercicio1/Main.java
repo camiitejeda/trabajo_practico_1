53563522f5e996f3fc549617ab91dc3752c042bc
Cami <Cami@LAPTOP-GRDN8A38>
2024-04-10 14:31:17 -0300

    Ejercicio 01 - Camila Tejeda

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float primerNumero = 8;
+		float segundoNumero = 5;
+		float tercerNumero = 1;
+		float cuartoNumero = 20;
+		float quintoNumero = 9;		
+		
+		float promedio = (primerNumero + segundoNumero + tercerNumero + cuartoNumero + quintoNumero)/ 5;
+		System.out.println(primerNumero+", "+segundoNumero);
+		System.out.println("El promedio es: "+ promedio );
+		
+	}
+
+}
