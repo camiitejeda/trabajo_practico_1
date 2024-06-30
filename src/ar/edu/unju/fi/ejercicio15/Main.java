0f4f78edc7435ca6eb843c5dda742399ae3de627
Cami <Cami@LAPTOP-GRDN8A38>
2024-06-30 19:48:51 -0300

    correcciones ejercicio 15 continuación

--- src/ar/edu/unju/fi/ejercicio15/Main.java
+++ src/ar/edu/unju/fi/ejercicio15/Main.java
@@ -3,34 +3,36 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-int numero = 0;
-		
-		while (numero < 5 || numero > 10) {
-			System.out.println("Ingrese un numero para determinar el espacio del arreglo en el rango [5,10]: ");
-			numero = scanner.nextInt();
-			if (numero < 5 || numero > 10) {
-				System.out.println("Valor fuera de rango");
-			}
-		}
-		
-		String[] arreglo = new String [numero]; 
+        int numero = 0;
 
-		 for (int i = 0; i < arreglo.length; i++) {
-	            System.out.print("Ingrese un número entero para la posición " + i + ": ");
-	            arreglo[i] = scanner.next();
-	        }
-		 
-	        System.out.println("Valores almacenados en el array:");
-	        for (int i = arreglo.length - 1 ; i >= 0; i--) {
-	            System.out.println("Índice " + i + ": " + arreglo[i]);
-	            scanner.close();
-	        }
+        while (numero < 5 || numero > 10) {
+            System.out.println("Ingrese un numero para determinar el espacio del arreglo en el rango [5,10]: ");
+            numero = scanner.nextInt();
+            if (numero < 5 || numero > 10) {
+                System.out.println("Valor fuera de rango");
+            }
+        }
 
-	
-	}
+        String[] arreglo = new String[numero];
 
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.print("Ingrese un nombre para la posición " + i + ": ");
+            arreglo[i] = scanner.next();
+        }
+
+        System.out.println("Valores almacenados en el array:");
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Índice " + i + ": " + arreglo[i]);
+        }
+
+        System.out.println("\nValores almacenados en el array en orden inverso:");
+        for (int i = arreglo.length - 1; i >= 0; i--) {
+            System.out.println("Índice " + i + ": " + arreglo[i]);
+        }
+
+        scanner.close();
+    }
 }
